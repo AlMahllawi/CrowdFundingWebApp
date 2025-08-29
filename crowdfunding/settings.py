836ee9361bb57c50d7d4be1f6bf7d678198bf388
Mohammad AlMahllawi <mohammadalmahllawi@gmail.com>
2025-08-29 18:32:29 +0300

    include python-decouple

--- crowdfunding/settings.py
+++ crowdfunding/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-97jiid8u0!nt!c=*xkmz(m!!i2re&$v1sqn7#%vwy&7gf19-o6'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-97jiid8u0!nt!c=*xkmz(m!!i2re&$v1sqn7#%vwy&7gf19-o6')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
