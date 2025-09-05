553fbe5627fd0a5f8f26893be715b39f2110a341
Mohammad AlMahllawi <mohammadalmahllawi@gmail.com>
2025-09-05 21:37:11 +0300

    Projects search

--- home/urls.py
+++ home/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 
 from . import views
 
-app_name="home"
+app_name = "home"
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.Search.as_view(), name="index"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,35 @@
+from django.db.models import Q
+from django.views.generic import ListView
 from django.shortcuts import render
+from projects.models import Project
+from .forms import SearchForm
 
-def index(request):
-    return render(request, 'home/index.html', { 'pagetitle': "Crowd-Funding Web Application" })
+
+class Search(ListView):
+    model = Project
+    template_name = "home/index.html"
+    context_object_name = "projects"
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get("query", "").strip()
+
+        if query:
+            query_words = query.split()
+            title_queries = Q()
+            tag_queries = Q()
+
+            for word in query_words:
+                title_queries |= Q(title__icontains=word)
+                tag_queries |= Q(tags__name__icontains=word)
+
+            queryset = queryset.filter(title_queries | tag_queries).distinct()
+
+        return queryset.order_by("-created_at")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = SearchForm(self.request.GET)
+        context["query"] = self.request.GET.get("query", "")
+        return context
